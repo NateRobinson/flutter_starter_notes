7069c2bd7521c13d28b6b89163eea1c86f47d7fb
NateRobinson <840501291@qq.com>
2019-03-24 12:22:55 +0800

    add image and icon

--- lib/ui/ImageAndIconAbout.dart
+++ lib/ui/ImageAndIconAbout.dart
@@ -11,39 +11,77 @@ class ImageAndIconAbout extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              '从 Asset 加载图片',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Text(
+                '从 Asset 加载图片',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
             ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            child: Text('通过 AssetImage'),
-            padding: EdgeInsets.all(10),
-          ),
-          Container(
-            child: Image(
-              image: AssetImage('imgs/avatar.png'),
-              width: 100,
-              height: 100,
+            Container(
+              child: Text('通过 AssetImage'),
+              padding: EdgeInsets.all(10),
             ),
-            padding: EdgeInsets.all(10),
-          ),
-          Container(
-            child: Text('or, 通过 Image.asset'),
-            padding: EdgeInsets.all(10),
-          ),
-          Container(
-            child: Image.asset('imgs/avatar.png', width: 100, height: 100),
-            padding: EdgeInsets.all(10),
-          ),
-        ],
+            Container(
+              child: Image(
+                image: AssetImage('imgs/avatar.png'),
+                width: 100,
+                height: 100,
+              ),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: Text('or, 通过 Image.asset'),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: Image.asset('imgs/avatar.png', width: 100, height: 100),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: Text(
+                '从网络加载图片',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              child: Text('通过 NetworkImage'),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: Image(
+                image: NetworkImage(
+                    'https://avatars2.githubusercontent.com/u/20411648?s=460&v=4'),
+                width: 100,
+                height: 100,
+              ),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: Text('or, 通过 Image.network'),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: Image.network(
+                  'https://avatars2.githubusercontent.com/u/20411648?s=460&v=4',
+                  color: Colors.blue,
+                  colorBlendMode: BlendMode.difference,
+                  width: 100,
+                  height: 100),
+              padding: EdgeInsets.all(10),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/TextAbout.dart
+++ lib/ui/TextAbout.dart
@@ -12,175 +12,177 @@ class TextAbout extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Text(
-              'Text 基础',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "左对齐" * 10,
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "右对齐" * 10,
-              textAlign: TextAlign.right,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "居中对齐" * 10,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "单行文本，超出部分以省略号结尾。" * 4,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "我被放大了1.5倍",
-              textScaleFactor: 1.5,
-            ),
-          ),
-          Container(
-            child: Text(
-              'TextStyle',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "自定义 TextStyle 的文字",
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18.0,
-                  // 【Note:】该属性用于指定行高，但它并不是一个绝对值，而是一个因子，具体的行高等于fontSize*height
-                  height: 1.2,
-                  fontFamily: "Courier",
-                  background: new Paint()..color = Colors.yellow,
-                  decoration: TextDecoration.underline,
-                  decorationStyle: TextDecorationStyle.dashed),
-            ),
-          ),
-          Container(
-            child: Text(
-              'TextSpan: 富文本的 Text ，可点击超链接之类',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text.rich(TextSpan(children: [
-              TextSpan(text: "Home: "),
-              TextSpan(
-                text: "https://flutterchina.club",
-                style: TextStyle(color: Colors.blue),
-                recognizer: new TapGestureRecognizer()
-                  ..onTap = () {
-                    print("click url");
-                  },
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Text(
+                'Text 基础',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               ),
-            ])),
-          ),
-          Container(
-            child: Text(
-              'DefaultTextStyle',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
             ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: DefaultTextStyle(
-              //1.设置文本默认样式
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
-              ),
-              textAlign: TextAlign.start,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("我的样式继承自 DefaultTextStyle"),
-                  Text("我的样式继承自 DefaultTextStyle"),
-                  Text(
-                    "我的样式我作主，不再继承 DefaultTextStyle",
-                    style: TextStyle(
-                        inherit: false, // 不继承默认样式
-                        color: Colors.grey),
-                  ),
-                ],
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "左对齐" * 10,
+                textAlign: TextAlign.left,
               ),
             ),
-          ),
-          Container(
-            child: Text(
-              '自定义字体-from asset',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "右对齐" * 10,
+                textAlign: TextAlign.right,
+              ),
             ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "Germania One Font",
-              style: TextStyle(fontFamily: 'RobotoCondensed', fontSize: 24),
-              textAlign: TextAlign.left,
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "居中对齐" * 10,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          Container(
-            child: Text(
-              '自定义字体-from package',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "单行文本，超出部分以省略号结尾。" * 4,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.left,
+              ),
             ),
-            color: Colors.grey[200],
-            height: 30,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 10),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            child: Text(
-              "在 https://pub.dartlang.org 找了一圈没有找到合适的库来进行这个 Demo",
-              textAlign: TextAlign.left,
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "我被放大了1.5倍",
+                textScaleFactor: 1.5,
+              ),
             ),
-          ),
-        ],
+            Container(
+              child: Text(
+                'TextStyle',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "自定义 TextStyle 的文字",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18.0,
+                    // 【Note:】该属性用于指定行高，但它并不是一个绝对值，而是一个因子，具体的行高等于fontSize*height
+                    height: 1.2,
+                    fontFamily: "Courier",
+                    background: new Paint()..color = Colors.yellow,
+                    decoration: TextDecoration.underline,
+                    decorationStyle: TextDecorationStyle.dashed),
+              ),
+            ),
+            Container(
+              child: Text(
+                'TextSpan: 富文本的 Text ，可点击超链接之类',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text.rich(TextSpan(children: [
+                TextSpan(text: "Home: "),
+                TextSpan(
+                  text: "https://flutterchina.club",
+                  style: TextStyle(color: Colors.blue),
+                  recognizer: new TapGestureRecognizer()
+                    ..onTap = () {
+                      print("click url");
+                    },
+                ),
+              ])),
+            ),
+            Container(
+              child: Text(
+                'DefaultTextStyle',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: DefaultTextStyle(
+                //1.设置文本默认样式
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20.0,
+                ),
+                textAlign: TextAlign.start,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("我的样式继承自 DefaultTextStyle"),
+                    Text("我的样式继承自 DefaultTextStyle"),
+                    Text(
+                      "我的样式我作主，不再继承 DefaultTextStyle",
+                      style: TextStyle(
+                          inherit: false, // 不继承默认样式
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                '自定义字体-from asset',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "Germania One Font",
+                style: TextStyle(fontFamily: 'RobotoCondensed', fontSize: 24),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Container(
+              child: Text(
+                '自定义字体-from package',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              color: Colors.grey[200],
+              height: 30,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              child: Text(
+                "在 https://pub.dartlang.org 找了一圈没有找到合适的库来进行这个 Demo",
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

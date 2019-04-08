f97bd792ff2b487e876b3e3564fc59c0a90228fd
NateRobinson <840501291@qq.com>
2019-04-08 18:23:06 +0800

    finish padding

--- lib/ui/PaddingAbout.dart
+++ lib/ui/PaddingAbout.dart
@@ -16,6 +16,50 @@ class PaddingAbout extends StatelessWidget {
         child: Column(
           children: <Widget>[
             CommonTitle('Padding 介绍'),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(16),
+              child: Text("Padding 可以给其子节点添加补白（填充）。"),
+            ),
+            Padding(
+              //上下左右各添加16像素补白
+              padding: EdgeInsets.all(16.0),
+              child: Container(
+                width: double.infinity,
+                color: Colors.blue,
+                child: Column(
+                  //显式指定对齐方式为左对齐，排除对齐干扰
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      color: Colors.red,
+                      child: Padding(
+                        //左边添加8像素补白
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text("左 padding 8"),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      child: Padding(
+                        //上下各添加8像素补白
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text("上下 padding 8"),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.pink,
+                      child: Padding(
+                        // 分别指定四个方向的补白
+                        padding:
+                            const EdgeInsets.fromLTRB(20.0, .0, 20.0, 20.0),
+                        child: Text("左上右下 padding ： 20，0，20，20"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),

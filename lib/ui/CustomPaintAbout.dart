4fb26fb01d37378150b835d2b0ffe4e742ef2e3c
NateRobinson <840501291@qq.com>
2019-04-16 10:19:25 +0800

    finish custom paint

--- lib/ui/CustomPaintAbout.dart
+++ lib/ui/CustomPaintAbout.dart
@@ -1,6 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_starter_notes/component/CommonTitle.dart';
-import 'dart:math';
 
 class CustomPaintAbout extends StatelessWidget {
   CustomPaintAbout({Key key, this.title});
@@ -17,14 +18,24 @@ class CustomPaintAbout extends StatelessWidget {
         child: Column(
           children: <Widget>[
             CommonTitle('CustomPaint 示例'),
-            CustomPaintRoute()
+            CustomPaintRoute(),
+            CommonTitle('CustomPaint 性能与优化'),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                  """1. 尽可能的利用好shouldRepaint返回值；在UI树重新build时，控件在绘制前都会先调用该方法以确定是否有必要重绘；假如我们绘制的UI不依赖外部状态，那么就应该始终返回false，因为外部状态改变导致重新build时不会影响我们的UI外观；如果绘制依赖外部状态，那么我们就应该在shouldRepaint中判断依赖的状态是否改变，如果已改变则应返回true来重绘，反之则应返回false不需要重绘。"""),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: Text(
+                  """2. 绘制尽可能多的分层；在上面五子棋的示例中，我们将棋盘和棋子的绘制放在了一起，这样会有一个问题：由于棋盘始终是不变的，用户每次落子时变的只是棋子，但是如果按照上面的代码来实现，每次绘制棋子时都要重新绘制一次棋盘，这是没必要的。优化的方法就是将棋盘单独抽为一个Widget，并设置其shouldRepaint回调值为false，然后将棋盘Widget作为背景。然后将棋子的绘制放到另一个Widget中，这样落子时只需要绘制棋子。"""),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
 
 class CustomPaintRoute extends StatelessWidget {
   @override
